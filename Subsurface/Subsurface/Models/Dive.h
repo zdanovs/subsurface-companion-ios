12f5da3760bfbd74374d1877b9d41ade0df9cd52
Andrey Zhdanov <andrjufka@gmail.com>
2014-06-01 01:28:47 +0300

    Integrate DB for local dives saving
    Signed-off-by: Andrey Zhdanov <andrjufka@gmail.com>

---
+++ Subsurface/Subsurface/Models/Dive.h
@@ -0,0 +1,21 @@
+//
+//  Dive.h
+//  Subsurface
+//
+//  Created by Andrey Zhdanov on 01/06/14.
+//  Copyright (c) 2014 Subsurface. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Dive : NSManagedObject
+
+@property (nonatomic, retain) NSString *name;
+@property (nonatomic, retain) NSNumber *latitude;
+@property (nonatomic, retain) NSNumber *longitude;
+@property (nonatomic, retain) NSDate *date;
+@property (nonatomic, retain) NSNumber *uploaded;
+
+@end
---
+++ Subsurface/Subsurface/Models/Dive.m
@@ -0,0 +1,20 @@
+//
+//  Dive.m
+//  Subsurface
+//
+//  Created by Andrey Zhdanov on 01/06/14.
+//  Copyright (c) 2014 Subsurface. All rights reserved.
+//
+
+#import "Dive.h"
+
+
+@implementation Dive
+
+@dynamic name;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic date;
+@dynamic uploaded;
+
+@end

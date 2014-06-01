972b121b2b6187ded6c8ef368fe25baaabc17535
Andrey Zhdanov <andrjufka@gmail.com>
2014-06-01 11:42:59 +0300

    Rename dive entity
    Signed-off-by: Andrey Zhdanov <andrjufka@gmail.com>

--- Subsurface/Subsurface/Models/SDive.h
+++ Subsurface/Subsurface/Models/SDive.h
@@ -1,5 +1,5 @@
 //
-//  Dive.h
+//  SDive.h
 //  Subsurface
 //
 //  Created by Andrey Zhdanov on 01/06/14.
@@ -10,12 +10,12 @@
 #import <CoreData/CoreData.h>
 
 
-@interface Dive : NSManagedObject
+@interface SDive : NSManagedObject
 
-@property (nonatomic, retain) NSString *name;
+@property (nonatomic, retain) NSDate *date;
 @property (nonatomic, retain) NSNumber *latitude;
 @property (nonatomic, retain) NSNumber *longitude;
-@property (nonatomic, retain) NSDate *date;
+@property (nonatomic, retain) NSString *name;
 @property (nonatomic, retain) NSNumber *uploaded;
 
 @end
--- Subsurface/Subsurface/Models/SDive.m
+++ Subsurface/Subsurface/Models/SDive.m
@@ -1,20 +1,20 @@
 //
-//  Dive.m
+//  SDive.m
 //  Subsurface
 //
 //  Created by Andrey Zhdanov on 01/06/14.
 //  Copyright (c) 2014 Subsurface. All rights reserved.
 //
 
-#import "Dive.h"
+#import "SDive.h"
 
 
-@implementation Dive
+@implementation SDive
 
-@dynamic name;
+@dynamic date;
 @dynamic latitude;
 @dynamic longitude;
-@dynamic date;
+@dynamic name;
 @dynamic uploaded;
 
 @end

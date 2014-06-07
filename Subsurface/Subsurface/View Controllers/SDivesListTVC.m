b35335644edf90a9ca5febd966fbd4bb5d8cdc85
Andrey Zhdanov <andrjufka@gmail.com>
2014-06-07 14:57:06 +0300

    Move "add new dive" to table header instead displaying it as first table row.
    Signed-off-by: Andrey Zhdanov <andrjufka@gmail.com>

--- Subsurface/Subsurface/View Controllers/SDivesListTVC.m
+++ Subsurface/Subsurface/View Controllers/SDivesListTVC.m
@@ -8,8 +8,9 @@
 
 #import "SDivesListTVC.h"
 #import "SDiveTableCell.h"
-#import "SAddDiveTableCell.h"
 #import "SCoreDiveService.h"
+
+#define kCellHeight    44.0f
 
 @interface SDivesListTVC ()
 
@@ -22,7 +23,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
     self.refreshControl = [[UIRefreshControl alloc] init];
@@ -46,33 +47,28 @@
     [super viewWillAppear:animated];
     
     self.navigationController.navigationBarHidden = NO;
-    self.tableView.contentOffset = CGPointMake(0, 44);
+    self.tableView.contentOffset = CGPointMake(0, kCellHeight);
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return _divesList.count + 1;
+    return _divesList.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *commonCell = nil;
     
-    if (indexPath.row == 0) {
-        SAddDiveTableCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AddDiveCell" forIndexPath:indexPath];
-        commonCell = cell;
-    } else {
-        SDiveTableCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DiveCell" forIndexPath:indexPath];
-        [cell setupDiveCell:_divesList[indexPath.row - 1]];
-        commonCell = cell;
-    }
+    SDiveTableCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DiveCell" forIndexPath:indexPath];
+    [cell setupDiveCell:_divesList[indexPath.row]];
+    commonCell = cell;
     
     return commonCell;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        SDive *dive = _divesList[indexPath.row - 1];
+        SDive *dive = _divesList[indexPath.row];
         
         NSMutableArray *mutableDivesList = [_divesList mutableCopy];
         [mutableDivesList removeObject:dive];
@@ -83,13 +79,37 @@
     }
 }
 
-- (UITableViewCellEditingStyle)tableView:(UITableView *)aTableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
     // Detemine if it's in editing mode
     if (self.tableView.editing) {
         return UITableViewCellEditingStyleNone;
     }
     
     return UITableViewCellEditingStyleDelete;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIToolbar *bkgToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.tableView.frame.size.width, kCellHeight + 1)];
+
+    UIButton *addDiveButton = [[UIButton alloc] initWithFrame:bkgToolbar.frame];
+    addDiveButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:14.0f];
+    [addDiveButton setTitle:NSLocalizedString(@"Add Dive", @"") forState:UIControlStateNormal];
+    [addDiveButton addTarget:self action:@selector(addNewDiveAction) forControlEvents:UIControlEventTouchUpInside];
+    [addDiveButton setTitleColor:[UIColor colorWithRed:0 green:0.478431 blue:1.0 alpha:1.0] forState:UIControlStateNormal];
+    
+    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0, kCellHeight, self.tableView.frame.size.width, 1)];
+    separatorView.backgroundColor = [UIColor colorWithRed:224/255.0 green:224/255.0 blue:224/255.0 alpha:1.0];
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:bkgToolbar.frame];
+    [headerView addSubview:bkgToolbar];
+    [headerView addSubview:addDiveButton];
+    [headerView addSubview:separatorView];
+    
+    return headerView;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return kCellHeight + 1;
 }
 
 #pragma mark - Additional actions
@@ -107,10 +127,32 @@
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated {
     [super setEditing:editing animated:animated];
     
-    if (editing) {
-        // Your code for entering edit mode goes here
-    } else {
-        // Your code for exiting edit mode goes here
+    self.tableView.allowsMultipleSelectionDuringEditing = editing;
+    [self.tableView reloadData];
+}
+
+- (void)addNewDiveAction {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Enter dive name", "")
+                                                        message:@""
+                                                       delegate:self
+                                              cancelButtonTitle:NSLocalizedString(@"Cancel", "")
+                                              otherButtonTitles:NSLocalizedString(@"Add", ""), nil];
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    
+    UITextField *textField = [alertView textFieldAtIndex:0];
+    textField.keyboardType = UIKeyboardTypeEmailAddress;
+    textField.textAlignment = NSTextAlignmentCenter;
+    textField.autocapitalizationType = UITextAutocapitalizationTypeSentences;
+    textField.placeholder = @"dive name";
+    
+    [alertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        UITextField *alertTextField = [alertView textFieldAtIndex:0];
+        
+        [SWEB addDive:alertTextField.text];
     }
 }
 

e5ffc3abd96ebc5f4f180309584426daae41df58
Andrey Zhdanov <andrjufka@gmail.com>
2014-06-15 18:19:29 +0300

    Dive details screen: prepare UI for dive edit mode.
    Signed-off-by: Andrey Zhdanov <andrjufka@gmail.com>

--- Subsurface/Subsurface/View Controllers/SDiveDetailsVC.m
+++ Subsurface/Subsurface/View Controllers/SDiveDetailsVC.m
@@ -21,6 +21,14 @@
 @property (weak, nonatomic) IBOutlet UILabel *diveLatitudeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *diveLongitudeLabel;
 
+@property (weak, nonatomic) IBOutlet UIView *editableInfoContainer;
+@property (weak, nonatomic) IBOutlet UIImageView *editableNameUnderline;
+@property (weak, nonatomic) IBOutlet UITextField *editableNameLabel;
+@property (weak, nonatomic) IBOutlet UITextField *editableDateLabel;
+@property (weak, nonatomic) IBOutlet UITextField *editableTimeLabel;
+@property (weak, nonatomic) IBOutlet UITextField *editableLatitudeLabel;
+@property (weak, nonatomic) IBOutlet UITextField *editableLongitudeLabel;
+
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIImageView *circleBackgroundImageView;
 @property (weak, nonatomic) IBOutlet UIView *tapAnimationView;
@@ -44,6 +52,12 @@
     
     self.diveLatitudeLabel.text = [NSString stringWithFormat:@"%@", self.dive.latitude];
     self.diveLongitudeLabel.text = [NSString stringWithFormat:@"%@", self.dive.longitude];
+    
+    self.editableNameLabel.text = self.diveNameLabel.text;
+    self.editableDateLabel.text = self.diveDateLabel.text;
+    self.editableTimeLabel.text = self.diveTimeLabel.text;
+    self.editableLatitudeLabel.text = self.diveLatitudeLabel.text;
+    self.editableLongitudeLabel.text = self.diveLongitudeLabel.text;
     
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(showMap)];
     [self.tapAnimationView addGestureRecognizer:tapRecognizer];
@@ -80,7 +94,7 @@
 }
 
 - (void)animateMapAppear:(BOOL)show {
-    UIBarButtonItem *closeMapButton = [[UIBarButtonItem alloc] initWithTitle:@"Close Map"
+    UIBarButtonItem *closeMapButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Close Map", "")
                                                                        style:UIBarButtonItemStylePlain
                                                                       target:self
                                                                       action:@selector(closeMap)];
@@ -102,7 +116,31 @@
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated {
     [super setEditing:editing animated:animated];
     
+    self.diveInfoContainer.hidden = editing;
+    self.editableInfoContainer.hidden = !editing;
     
+    if (editing) {
+        self.editableNameLabel.text = self.diveNameLabel.text;
+        self.editableDateLabel.text = self.diveDateLabel.text;
+        self.editableTimeLabel.text = self.diveTimeLabel.text;
+        self.editableLatitudeLabel.text = self.diveLatitudeLabel.text;
+        self.editableLongitudeLabel.text = self.diveLongitudeLabel.text;
+        
+        [self.editableNameLabel becomeFirstResponder];
+    } else {
+        self.diveNameLabel.text = self.editableNameLabel.text;
+        self.diveDateLabel.text = self.editableDateLabel.text;
+        self.diveTimeLabel.text = self.editableTimeLabel.text;
+        self.diveLatitudeLabel.text = self.editableLatitudeLabel.text;
+        self.editableLongitudeLabel.text = self.editableLongitudeLabel.text;
+        
+        [self.view endEditing:YES];
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+    [self setEditing:NO animated:YES];
 }
 
 @end
